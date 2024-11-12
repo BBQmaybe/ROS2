82a2273a78d9b6db410f8d56b5151fb65ef69807
BBQmaybe <144081429+BBQmaybe@users.noreply.github.com>
2024-11-13 00:34:08 +0700

    ฅ^•ﻌ•^ฅ

--- Module6/ex03/src/robot_bringup/launch/robot_lidar.launch.py
+++ Module6/ex03/src/robot_bringup/launch/robot_lidar.launch.py
@@ -78,7 +78,7 @@ def generate_launch_description():
     rviz = Node(
        package='rviz2',
        executable='rviz2',
-       arguments=['-d', os.path.join(pkg_project_bringup, 'config', 'imu.rviz')],
+       arguments=['-d', os.path.join(pkg_project_bringup, 'config', 'diff_drive.rviz')],
        condition=IfCondition(LaunchConfiguration('rviz'))
     )
 
